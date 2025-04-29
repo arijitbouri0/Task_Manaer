718955ca52182a2f527677d5d1ac4f39b73d42a5
Arijit Bouri <137565275+arijitbouri0@users.noreply.github.com>
2025-04-29 07:10:37 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,7 +7,6 @@ import toast from 'react-hot-toast';
 const Navbar = () => {
   const { user } = useSelector((state) => state.auth);
   const dispatch=useDispatch()
-  console.log(user.userName);
   const logoutHandler = async () => {
     try {
       const { data } = await axios.get(`http://localhost:5000/api/auth/logout/`, { withCredentials: true });
@@ -21,7 +20,7 @@ const Navbar = () => {
     <div className="w-full flex justify-between items-center p-4 bg-gray-800 text-white">
       <div className="text-xl font-bold">Task Manager</div>
       <div className="flex items-center space-x-4">
-        <span>{user.userName}</span>
+        <span>{user.email}</span>
         <button
           onClick={logoutHandler}
           className="bg-red-500 text-white px-4 py-2 rounded cursor-pointer"
